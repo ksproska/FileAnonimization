b1f5d0b07f63dd414f633f6505b83729b4e8bb5f
Kamila Sproska <kamila.sproska@gmail.com>
2023-10-13 09:28:02 +0200

    reading text and pretending to do sth with it

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string workingDirectory = Environment.CurrentDirectory;
+string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+string filepath = Path.Combine(projectDirectory, "sample.txt");
+
+string readText = File.ReadAllText(filepath);
+Console.WriteLine("\"Original\":   " + readText);
+
+string ProcessData(string originalInput)
+{
+    // do sth
+    return originalInput;
+}
+
+string processedText = ProcessData(readText);
+Console.WriteLine("\"Anonymized\": " + processedText);
