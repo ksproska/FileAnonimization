a06d1850681589c83fc25d5b9156ae91efa51a3a
Kamila Sproska <kamila.sproska@gmail.com>
2023-10-13 10:10:05 +0200

    added anonymization of date, pesel and phone number

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿string GetTextFromFile()
+﻿using System.Text.RegularExpressions;
+
+string GetTextFromFile()
 {
     string workingDirectory = Environment.CurrentDirectory;
     string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
@@ -8,10 +10,40 @@
     return readText;
 }
 
-string ProcessData(string originalInput)
+bool DoesContainSensitiveInformation(string word)
 {
-    // do sth
-    return originalInput;
+    if (IsPhoneNumber(word)) return true;
+    if (IsPesel(word)) return true;
+    if (IsDate(word)) return true;
+    return false;
+}
+
+bool IsPhoneNumber(string text)
+{
+    return Regex.IsMatch(text, @"^[0-9]{9}$");
+}
+
+bool IsPesel(string text)
+{
+    return Regex.IsMatch(text, @"^[0-9]{11}$");
+}
+
+bool IsDate(string text)
+{
+    return Regex.IsMatch(text, @"^(\d{1,2})\.(\d{1,2})\.(\d{4})$");
+}
+
+string ProcessData(string text)
+{
+    var punctuation = text.Where(Char.IsPunctuation).Distinct().ToArray();
+    var words = text.Split().Select(x => x.Trim(punctuation));
+    var wordsToAnonimize = words.Where(x => DoesContainSensitiveInformation(x));
+    
+    foreach (string word in wordsToAnonimize)
+    {
+        text = text.Replace(word, new String('*', word.Length));
+    }
+    return text;
 }
 
 string readText = GetTextFromFile();
