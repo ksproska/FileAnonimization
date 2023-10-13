735c44412caebfe34c5dd2aa31cf4b280e7b1290
Kamila Sproska <kamila.sproska@gmail.com>
2023-10-13 09:32:17 +0200

    refactor

--- Program.cs
+++ Program.cs
@@ -1,15 +1,21 @@
-﻿string workingDirectory = Environment.CurrentDirectory;
-string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
-string filepath = Path.Combine(projectDirectory, "sample.txt");
-
-string readText = File.ReadAllText(filepath);
-Console.WriteLine("\"Original\":   " + readText);
+﻿string GetTextFromFile()
+{
+    string workingDirectory = Environment.CurrentDirectory;
+    string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+    string filepath = Path.Combine(projectDirectory, "sample.txt");
+    
+    string readText = File.ReadAllText(filepath);
+    return readText;
+}
 
 string ProcessData(string originalInput)
 {
     // do sth
     return originalInput;
 }
+
+string readText = GetTextFromFile();
+Console.WriteLine("\"Original\":   " + readText);
 
 string processedText = ProcessData(readText);
 Console.WriteLine("\"Anonymized\": " + processedText);
