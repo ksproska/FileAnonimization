99686d0925c34dc9a3af9862608bf5e26a6ad7f4
Kamila Sproska <kamila.sproska@gmail.com>
2023-10-15 20:05:23 +0200

    changing project to test project

--- FileAnonimizationWithTests/Anonimizator.cs
+++ FileAnonimizationWithTests/Anonimizator.cs
@@ -1,6 +1,7 @@
+using System;
+using System.Linq;
 using System.Text.RegularExpressions;
 
-namespace FileAnonimization;
 
 public class Anonimizator
 {
---
+++ FileAnonimizationWithTests/Tests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace FileAnonimizationWithTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        string GetTextFromFile()
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+            string filepath = Path.Combine(projectDirectory, "FileAnonimizationWithTests", "sample.txt");
+
+            string readText = File.ReadAllText(filepath);
+            return readText;
+        }
+
+        [Test]
+        public void Test1()
+        {
+            string readText = GetTextFromFile();
+            Console.WriteLine("\"Original\":   " + readText);
+
+            var anonimizator = new Anonimizator();
+            string processedText = anonimizator.Anonimize(readText);
+            Console.WriteLine("\"Anonymized\": " + processedText);
+        }
+    }
+}
