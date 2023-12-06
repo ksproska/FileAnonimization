7b5310eca641e2c3a48abcb54790bed95bc0f403
Kamila Sproska <61067969+ksproska@users.noreply.github.com>
2023-12-06 11:55:18 +0100

    added file path, improving GUI

--- FileAnonimizationWpfVS/MainWindow.xaml.cs
+++ FileAnonimizationWpfVS/MainWindow.xaml.cs
@@ -1,4 +1,3 @@
-using DocumentFormat.OpenXml.Office2016.Presentation.Command;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -10,13 +9,10 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
-using exporttoword = Microsoft.Office.Interop.Word;
+using ExportToWord = Microsoft.Office.Interop.Word;
 
 namespace FileAnonimizationWpfVS
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         private ContextFinder _contextFinder;
@@ -32,34 +28,41 @@ namespace FileAnonimizationWpfVS
         ObservableCollection<string> unselectedElement;
         string text = "";
         private static (string, string)[] wordsToAnonimize;
+        private ObservableCollection<string> listBeforeItemsSource;
+        private ObservableCollection<string> listAfterItemsSource;
+
         public MainWindow()
         {
             InitializeComponent();
 
             _contextFinder = new ContextFinder(
-                new []{ "jest", "był", "była", "ma", "miał", "miała"}
-                );
-            _contextIlnessFinder = new ContextIlnessFinder((new[] { "choruje na", "chorował na", "chorowała na",
-                "cierpi na", "ma objawy", "wykazuje objawy", "chory na"}).ToList().Select(x => x.ToLower()).ToList());
+                new[] { "jest", "był", "była", "ma", "miał", "miała" }
+            );
+            _contextIlnessFinder = new ContextIlnessFinder((new[]
+            {
+                "choruje na", "chorował na", "chorowała na",
+                "cierpi na", "ma objawy", "wykazuje objawy", "chory na"
+            }).ToList().Select(x => x.ToLower()).ToList());
             string workingDirectory = Environment.CurrentDirectory;
             string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
             string namesPath = System.IO.Path.Combine(projectDirectory, "data", "names.csv");
             string surnamesPath = System.IO.Path.Combine(projectDirectory, "data", "surnames.csv");
             var csvDataReader = new CsvDataReader(namesPath, surnamesPath);
-            _sensitiveDataFinder = new SensitiveDataFinder(csvDataReader.GetNames(), csvDataReader.GetSurnames(), _contextFinder, _contextIlnessFinder);
+            _sensitiveDataFinder = new SensitiveDataFinder(csvDataReader.GetNames(), csvDataReader.GetSurnames(),
+                _contextFinder, _contextIlnessFinder);
             _sensitiveDataCensor = new SensitiveDataCensor();
             _dictionary = new Dictionary<string, string>()
             {
-                {"name", "leave only first character"},
-                {"surname", "leave only first character"},
-                {"phone number", "censor last 6 digits"},
-                {"pesel", "censor last 7 digits"},
-                {"date", "leave only a year"},
-                {"suspected name or surname", "leave only first character"},
-                {"user selection", "stars"},
-                {"suspected ilness",  "leave only first character"},
-                {"address", "leave only first character"},
-                {"postal code", "leave only minus sign"}
+                { "name", "leave only first character" },
+                { "surname", "leave only first character" },
+                { "phone number", "censor last 6 digits" },
+                { "pesel", "censor last 7 digits" },
+                { "date", "leave only a year" },
+                { "suspected name or surname", "leave only first character" },
+                { "user selection", "stars" },
+                { "suspected ilness", "leave only first character" },
+                { "address", "leave only first character" },
+                { "postal code", "leave only minus sign" }
             };
             selectedElement = new ObservableCollection<string>();
             unselectedElement = new ObservableCollection<string>();
@@ -71,31 +74,27 @@ namespace FileAnonimizationWpfVS
             if (rectangle != null && e.LeftButton == MouseButtonState.Pressed)
             {
                 DragDrop.DoDragDrop(rectangle,
-                                     rectangle.Fill.ToString(),
-                                     DragDropEffects.Copy);
+                    rectangle.Fill.ToString(),
+                    DragDropEffects.Copy);
             }
         }
 
         private Brush _previousFill = null;
+
         private void ellipse_DragEnter(object sender, DragEventArgs e)
         {
-            Border rectangle = sender as Border;
-            if (rectangle != null)
+            Border border = sender as Border;
+            if (border != null)
             {
-                // Save the current Fill brush so that you can revert back to this value in DragLeave.
-                _previousFill = rectangle.Background;
-
-                // If the DataObject contains string data, extract it.
+                _previousFill = border.Background;
                 if (e.Data.GetDataPresent(DataFormats.StringFormat))
                 {
                     string dataString = (string)e.Data.GetData(DataFormats.StringFormat);
-
-                    // If the string can be converted into a Brush, convert it.
                     BrushConverter converter = new BrushConverter();
                     if (converter.IsValid(dataString))
                     {
                         Brush newFill = (Brush)converter.ConvertFromString(dataString);
-                        rectangle.Background = newFill;
+                        border.Background = newFill;
                     }
                 }
             }
@@ -104,13 +103,9 @@ namespace FileAnonimizationWpfVS
         private void ellipse_DragOver(object sender, DragEventArgs e)
         {
             e.Effects = DragDropEffects.None;
-
-            // If the DataObject contains string data, extract it.
             if (e.Data.GetDataPresent(DataFormats.StringFormat))
             {
                 string dataString = (string)e.Data.GetData(DataFormats.StringFormat);
-
-                // If the string can be converted into a Brush, allow copying.
                 BrushConverter converter = new BrushConverter();
                 if (converter.IsValid(dataString))
                 {
@@ -127,49 +122,52 @@ namespace FileAnonimizationWpfVS
                 rectangle.Background = _previousFill;
             }
         }
+
         private void ellipse_Drop(object sender, DragEventArgs e)
         {
             Border rectangle = sender as Border;
             if (rectangle != null)
             {
-                // If the DataObject contains string data, extract it.
                 if (e.Data.GetDataPresent(DataFormats.FileDrop))
                 {
-                    string[] dataString = (string[])e.Data.GetData(DataFormats.FileDrop);
-                    text = WordFileExtractor.UploadAndExtractWordFile(dataString[0]);
-                    
+                    string[] filePaths = (string[])e.Data.GetData(DataFormats.FileDrop);
+                    var firstFilePath = filePaths[0];
+                    filepathLabel.Text = firstFilePath;
+                    text = WordFileExtractor.UploadAndExtractWordFile(firstFilePath);
+
+
                     BrushConverter converter = new BrushConverter();
-                    if (converter.IsValid(dataString))
+                    if (converter.IsValid(filePaths))
                     {
-                        Brush newFill = (Brush)converter.ConvertFromString(dataString[0]);
+                        Brush newFill = (Brush)converter.ConvertFromString(firstFilePath);
                         rectangle.Background = newFill;
                     }
+
                     try
                     {
-                    
                         wordsToAnonimize = _sensitiveDataFinder.GetWordsToAnonimize(text);
-                        /*foreach ((string, string) sensData in wordsToAnonimize)
-                        {
-                            uploadedFile.Items.Add(sensData.Item1 + " - " + sensData.Item2 + " - " + _dictionary[sensData.Item2]);
-                        }*/
-                        
                         processedText = _sensitiveDataCensor.Anonymize(text, wordsToAnonimize, _dictionary);
                         FlowDocument flowDoc = new FlowDocument();
                         richTextBox.Document = flowDoc;
-                       
+
                         Paragraph paragraph = new Paragraph();
                         var words = wordsToAnonimize.Select(g => g.Item1);
-                        
+
                         var punctuation = text.Where(Char.IsPunctuation).Distinct().ToArray();
                         var wordsAll = text.Split().Select(x => x.Trim(punctuation));
                         var onlyWordsToAnonimize = wordsAll.Where(x => words.Contains(x));
 
-                        Paragraph finalParagraph = TextFormatter.FormatText(paragraph, text, onlyWordsToAnonimize.ToList());
+                        Paragraph finalParagraph =
+                            TextFormatter.FormatText(paragraph, text, onlyWordsToAnonimize.ToList());
                         flowDoc.Blocks.Add(finalParagraph);
 
                         string t = new TextRange(finalParagraph.ContentStart, paragraph.ContentEnd).Text;
 
-                        ListBefore.ItemsSource = words.Distinct().OrderBy(x => x).ToList();
+                        listBeforeItemsSource = new ObservableCollection<string>(words.Distinct().OrderBy(x => x).ToList());
+                        ListBefore.ItemsSource = listBeforeItemsSource;
+                        ListAfter.ItemsSource = listAfterItemsSource;
+                        ButtonPlus.IsEnabled = true;
+                        ApplySelected();
                     }
                     catch (Exception exception)
                     {
@@ -180,14 +178,51 @@ namespace FileAnonimizationWpfVS
                         paragraph.Inlines.Add(exception.ToString());
                         flowDoc.Blocks.Add(paragraph);
                     }
-                    
                 }
-                
             }
         }
 
-        //OK button
-        private void BtnOk_Click(object sender, RoutedEventArgs e)
+        private void Plus_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (ListBefore.SelectedItem != null)
+            {
+                var listBeforeSelectedItem = ListBefore.SelectedItem as string;
+                listBeforeItemsSource.Remove(listBeforeSelectedItem);
+                ListBefore.UnselectAll();
+                ListAfter.Items.Add(listBeforeSelectedItem);
+            }
+            else if (richTextBox.Selection != null && richTextBox.Selection.Text != "")
+            {
+                ListAfter.Items.Add(richTextBox.Selection.Text);
+                ListBefore.UnselectAll();
+            }
+            else if (ListBefore.SelectedItem == null || ListBefore.SelectedItem == "" ||
+                     richTextBox.Selection == null || richTextBox.Selection.Text != "")
+            {
+                MessageBox.Show("Select item");
+            }
+
+            ButtonMinus.IsEnabled = true;
+            ApplySelected();
+        }
+
+        private void Minus_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (ListAfter.SelectedItem != null)
+            {
+                var listAfterSelectedItem = ListAfter.SelectedItem as string;
+                ListAfter.Items.Remove(listAfterSelectedItem);
+                ListAfter.UnselectAll();
+                listBeforeItemsSource.Add(listAfterSelectedItem);
+            }
+            else
+            {
+                MessageBox.Show("Select item");
+            }
+            ApplySelected();
+        }
+
+        private void ApplySelected()
         {
             if (ListAfter.Items != null)
             {
@@ -197,89 +232,38 @@ namespace FileAnonimizationWpfVS
                 {
                     if (ListAfter.Items.GetItemAt(i) != null)
                     {
-                        var t = new Tuple<string, string>(ListAfter.Items.GetItemAt(i).ToString(), _sensitiveDataFinder.GetSensitiveInformationTypeForSelectedWords(ListAfter.Items.GetItemAt(i).ToString()));
+                        var t = new Tuple<string, string>(ListAfter.Items.GetItemAt(i).ToString(),
+                            _sensitiveDataFinder
+                                .GetSensitiveInformationTypeForSelectedWords(
+                                    ListAfter.Items.GetItemAt(i).ToString()
+                                )
+                        );
                         w.Add(t);
                     }
-
                 }
 
-                processedText = _sensitiveDataCensor.Anonymize(text, w.Select(x => (x.Item1, x.Item2)).ToArray(), _dictionary);
+                processedText =
+                    _sensitiveDataCensor.Anonymize(text, w.Select(x => (x.Item1, x.Item2)).ToArray(), _dictionary);
                 TextBox2.Text = processedText;
             }
             else
             {
                 MessageBox.Show("Select items");
             }
+
             TextBox2.Text = processedText;
-        }
-
-        //plus button
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-
-            if (ListBefore.SelectedItem != null)
-            {
-                ListAfter.Items.Add(ListBefore.SelectedItem as string);
-                ListBefore.UnselectAll();
-            }
-            else if (richTextBox.Selection != null && richTextBox.Selection.Text != "")
-            {
-                ListAfter.Items.Add(richTextBox.Selection.Text);
-                ListBefore.UnselectAll();
-            }
-            else if (ListBefore.SelectedItem == null || ListBefore.SelectedItem == "" || richTextBox.Selection == null || richTextBox.Selection.Text != "")
-            {
-                MessageBox.Show("Select item");
-            }
-        }
-
-        //minus button
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            if (ListAfter.SelectedItem != null)
-            {
-                ListAfter.Items.Remove(ListAfter.SelectedItem as string);
-                ListAfter.UnselectAll();
-            }
-            else
-            {
-                MessageBox.Show("Select item");
-            }
+            ButtonExport.IsEnabled = true;
         }
 
         private void Export_Click(object sender, RoutedEventArgs e)
         {
-            exporttoword.Application wordapp = new exporttoword.Application();
+            ExportToWord.Application wordapp = new ExportToWord.Application();
             wordapp.Visible = true;
-            exporttoword.Document worddoc;
+            ExportToWord.Document worddoc;
             object wordobj = System.Reflection.Missing.Value;
             worddoc = wordapp.Documents.Add(ref wordobj);
             wordapp.Selection.TypeText(TextBox2.Text);
             wordapp = null;
         }
-        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-
-        private void ListBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-
-        private void TextBox2_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        
-        
     }
 }
